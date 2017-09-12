794e03acb111f000fbf6268613b2a9f6a25f88c6
guohs <guohs126@126.com>
2017-09-12 19:32:33 +0800

    新增age属性

--- src/com/baidu/Hello.java
+++ src/com/baidu/Hello.java
@@ -1,10 +1,18 @@
 package com.baidu;
 
 public class Hello {
-    private String name1;
-
+    private String name;
+    private Integer age;
     public String getName() {
         return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
     public void setName(String name) {
