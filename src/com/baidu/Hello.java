f37f23c109c608a448c999700a6186eeac883818
guohs <guohs126@126.com>
2017-09-12 19:42:35 +0800

    新增address属性

--- src/com/baidu/Hello.java
+++ src/com/baidu/Hello.java
@@ -3,6 +3,7 @@ package com.baidu;
 public class Hello {
     private String name;
     private Integer age;
+    private String address;
     public String getName() {
         return name;
     }
@@ -17,6 +18,14 @@ public class Hello {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public void sayHello(){
