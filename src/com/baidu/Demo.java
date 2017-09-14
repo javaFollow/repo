c18b461668a064223fb912992dde65b191075132
guohs <guohs126@126.com>
2017-09-14 18:06:29 +0800

    启动线程

--- src/com/baidu/Demo.java
+++ src/com/baidu/Demo.java
@@ -1,7 +1,18 @@
 package com.baidu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo {
     public void test(){
         System.out.println("你好");
+    }
+
+    public static void main(String[] args) {
+        List<Integer> obj = new ArrayList<Integer>();
+        Thread thread1 = new Thread(new MyThread(obj));
+        Thread thread2 = new Thread(new HerThread(obj));
+        thread1.start();
+        thread2.start();
     }
 }
